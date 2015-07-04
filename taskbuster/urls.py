fdf9f90d8f259550f979a625ea956577dec51d1e
laurent jeuvrey <laurent.jeuvrey@orange.fr>
2015-07-04 22:18:49 +0200

    Added html base tempaltes

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from .views import home
+from .views import home, home_files
 
 urlpatterns = [
     # Examples:
@@ -10,4 +10,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
 ]
